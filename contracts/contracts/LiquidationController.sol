3aa008870092bab446e2e02888467c543ff466cb
Ben Clabby <benclabby@gmail.com>
2021-12-21 22:54:47 -0500

    Start to add AAVE Flash Loans

--- contracts/contracts/LiquidationController.sol
+++ contracts/contracts/LiquidationController.sol
@@ -1,70 +1,55 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import { FlashLoanReceiverBase, ILendingPoolAddressesProvider } from "./AaveInterfaces.sol";
+import { bdToken, Stabilizer } from "./BaoInterfaces.sol";
+import { IUniswapV2Router02 } from "./UniswapInterfaces.sol";
 
-abstract contract bdToken is IERC20 {
-    address public underlying;
-
-    function redeem(uint redeemTokens) virtual external returns (uint);
-    function liquidateBorrow(address borrower, uint repayAmount, address cTokenCollateral) virtual external returns (uint);
-}
-
-// Probably want to exchange this for 1inch or similar
-interface Router {
-    function swapExactTokensForTokens(
-        uint amountIn,
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external returns (uint[] memory amounts);
-}
-
-interface AaveLendingPool {
-    function flashLoan(
-        address receiverAddress,
-        address[] calldata assets,
-        uint256[] calldata amounts,
-        uint256[] calldata modes,
-        address onBehalfOf,
-        bytes calldata params,
-        uint16 referralCode
-    ) external;
-
-    function repay(
-        address asset,
-        uint256 amount,
-        uint256 rateMode,
-        address onBehalfOf
-    ) external;
-}
-
-interface Stabilizer {
-    function buy(uint amount) external;
-    function sell(uint amount) external;
-}
-
-contract LiquidationController is Ownable {
+contract LiquidationController is
+Ownable,
+FlashLoanReceiverBase(ILendingPoolAddressesProvider(0x506B0B2CF20FAA8f38a4E2B524EE43e1f4458Cc5))
+{
     bdToken immutable bdUSD;
-    AaveLendingPool immutable aaveLending;
     IERC20 immutable DAI;
     IERC20 immutable bUSD;
     Stabilizer immutable stabilizer;
-    Router immutable swapRouter;
+    IUniswapV2Router02 immutable swapRouter;
 
     bdToken[] public collateral;
 
     using SafeERC20 for IERC20;
+    using SafeMath for uint256;
 
     constructor() {
         bdUSD = bdToken(0x8584B05012749bdd32E41f8c7eB973D2283d1e56);
-        aaveLending = AaveLendingPool(0x9E5C7835E4b13368fd628196C4f1c6cEc89673Fa);
         DAI = IERC20(0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108);
         bUSD = IERC20(0xDF559301C178221E8D76E4A91126C504Dfe5947a);
         stabilizer = Stabilizer(0xD15C57FE113C6276FAD2F82658BB420351147f5E);
-        swapRouter = Router(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506); // SUSHI Router on Ropsten
+        swapRouter = IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506); // SUSHI Router on Ropsten
+    }
+
+    // This function is called after the contract has received the flash loaned amount
+    function executeOperation(
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        // Liquidation logic, contract has funds
+
+        // Approve tokens to be taken back by the lending pool
+        for (uint i = 0; i < assets.length; i++) {
+            uint amountOwed = amounts[i].add(premiums[i]);
+            IERC20(assets[i]).approve(address(LENDING_POOL), amountOwed);
+        }
+
+        return true;
     }
 
     /**
@@ -74,7 +59,12 @@ contract LiquidationController is Ownable {
       * @param _bdCollaterals The bdToken address of the collateral we want to claim
       * @param _totalRepayAmount The total amount of the synth assets that we plan to repay the user debts with
       */
-    function executeLiquidations(address[] memory _borrowers, uint256[] memory _repayAmounts, address[] memory _bdCollaterals, uint256 _totalRepayAmount) external {
+    function executeLiquidations(
+        address[] memory _borrowers,
+        uint256[] memory _repayAmounts,
+        address[] memory _bdCollaterals,
+        uint256 _totalRepayAmount
+    ) external {
         // Get a flash loan for DAI
         address[] memory assets = new address[](1);
         assets[0] = address(DAI);
@@ -82,12 +72,14 @@ contract LiquidationController is Ownable {
         amounts[0] = _totalRepayAmount;
         uint256[] memory modes = new uint256[](1);
         modes[0] = 0;
-        aaveLending.flashLoan(address(this), assets, amounts, modes, address(0), "", 0);
+        LENDING_POOL.flashLoan(address(this), assets, amounts, modes, address(this), "", 0);
+
+        // TODO- Move logic - https://docs.aave.com/developers/guides/flash-loans#2.-calling-flashloan
 
         // Approve transfer of DAI to stabilizer
         DAI.approve(address(stabilizer), _totalRepayAmount);
 
-        // Exchange for bUSD on Stabilzer
+        // Exchange for bUSD on Stabilizer
         stabilizer.buy(_totalRepayAmount);
 
         // Approve bUSD transfers for liquidations
@@ -117,10 +109,6 @@ contract LiquidationController is Ownable {
 
         // Sell remaining bUSD
         stabilizer.sell(bUSD.balanceOf(address(this)));
-
-        // Repay loan
-        DAI.approve(address(aaveLending), type(uint256).max);
-        aaveLending.repay(address(DAI), type(uint256).max, 0, address(this)); // Don't think we should be using type(uint256).max here for the amount
     }
 
     function addCollateralOption(address _newCollateral) external onlyOwner {
@@ -137,7 +125,6 @@ contract LiquidationController is Ownable {
 
     function retrieve(address token, uint256 amount) external onlyOwner {
         IERC20 tokenContract = IERC20(token);
-        require(tokenContract.balanceOf(address(this)) >= amount, "Token balance insufficient");
         tokenContract.safeTransfer(msg.sender, amount);
     }
 
